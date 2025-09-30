ca3b24cea610276fc94d64048146e45e91d45927
Jiii-Eun <mh030231@gmail.com>
2025-09-30 15:34:54 +0900

    Remove: 남아있는 e2e설정 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import TestMotions from "@/app/components/TestMotion";
+import { cn } from "@/lib/cn";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1
+        className={cn(
+          "p-4 rounded-lg",
+          "bg-blue-600 text-purple-900",
+          "text-3xl font bold",
+          "hover:shadow-lg transition-all",
+        )}
+      >
+        Home
+      </h1>
+      <TestMotions />
     </div>
   );
 }
