c8f708693f90195945e4207dee852f445c551d14
Jiii-Eun <mh030231@gmail.com>
2025-09-30 14:20:24 +0900

    Remove: 테스트 파일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-// 테스트용
-import Link from "next/link";
-
-import TestMotions from "@/app/test/TestMotions";
-
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Link href="/about">About</Link>
-      <TestMotions />
     </div>
   );
 }
--- src/lib/api/test/api.ts
+++
@@ -1,3 +0,0 @@
-export async function fetchGreeting() {
-  return { message: "Hello from API" };
-}
