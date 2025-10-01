22153745b5aeb1e9648d7df4737b4638232deb36
Jiii-Eun <mh030231@gmail.com>
2025-10-01 14:42:54 +0900

    Feat: 아이콘 관리

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,22 @@ const nextConfig: NextConfig = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/, // tsx, jsx 안에서만 적용
-      use: ["@svgr/webpack"],
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeAttrs",
+                  params: { attrs: "(fill|stroke)" }, // 고정 색상 제거
+                },
+              ],
+            },
+          },
+        },
+      ],
     });
 
     return config;
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,99 @@
+// 브랜드(logo)
+import LogoBig from "@/assets/brand/logo-big.svg";
+import LogoKakao from "@/assets/brand/logo-kakao.svg";
+import LogoSmall from "@/assets/brand/logo-small.svg";
+// arrow
+import ArrowDownFill from "@/assets/icons/arrows/arrow-down-fill.svg";
+import ArrowDown from "@/assets/icons/arrows/arrow-down.svg";
+import ArrowLeftFillDisabled from "@/assets/icons/arrows/arrow-left-fill-disabled.svg";
+import ArrowLeftFill from "@/assets/icons/arrows/arrow-left-fill.svg";
+import ArrowLeft from "@/assets/icons/arrows/arrow-left.svg";
+import ArrowRightFillDisabled from "@/assets/icons/arrows/arrow-right-fill-disabled.svg";
+import ArrowRightFill from "@/assets/icons/arrows/arrow-right-fill.svg";
+import ArrowRight from "@/assets/icons/arrows/arrow-right.svg";
+// buttons
+import ButtonAddImage from "@/assets/icons/buttons/button-add-image.svg";
+import ButtonAdd from "@/assets/icons/buttons/button-add.svg";
+import ButtonEdit from "@/assets/icons/buttons/button-edit.svg";
+import ButtonRemove from "@/assets/icons/buttons/button-remove.svg";
+// Me / SNS
+import MeRegister from "@/assets/icons/me-sns/me-register.svg";
+import MeReservations from "@/assets/icons/me-sns/me-reservations.svg";
+import MeSchedule from "@/assets/icons/me-sns/me-schedule.svg";
+import Me from "@/assets/icons/me-sns/me.svg";
+import SnsFacebook from "@/assets/icons/me-sns/sns-facebook.svg";
+import SnsInstagram from "@/assets/icons/me-sns/sns-instagram.svg";
+import SnsX from "@/assets/icons/me-sns/sns-x.svg";
+import SnsYoutube from "@/assets/icons/me-sns/sns-youtube.svg";
+// Status
+import CheckMarkFill from "@/assets/icons/status/check-mark-fill.svg";
+import CheckMark from "@/assets/icons/status/check-mark.svg";
+import CloseFill from "@/assets/icons/status/close-fill.svg";
+import Close from "@/assets/icons/status/close.svg";
+import DotSmallBlue from "@/assets/icons/status/dot-small-blue.svg";
+import DotSmallGreen from "@/assets/icons/status/dot-small-green.svg";
+import DotSmallRed from "@/assets/icons/status/dot-small-red.svg";
+import StarEmpty from "@/assets/icons/status/star-empty.svg";
+import StarFill from "@/assets/icons/status/star-fill.svg";
+import VisibilityOff from "@/assets/icons/status/visibility-off.svg";
+import Visibility from "@/assets/icons/status/visibility.svg";
+// UI
+import Calendar from "@/assets/icons/ui/calendar.svg";
+import MenuDot from "@/assets/icons/ui/menu-dot.svg";
+import NotificationBell from "@/assets/icons/ui/notification-bell.svg";
+import NotingPage from "@/assets/icons/ui/noting-page.svg";
+import Search from "@/assets/icons/ui/search.svg";
+
+// 아이콘 모음
+export const Icons = {
+  // 브랜드
+  LogoBig,
+  LogoKakao,
+  LogoSmall,
+
+  // 화살표
+  ArrowDown,
+  ArrowDownFill,
+  ArrowLeft,
+  ArrowLeftFill,
+  ArrowLeftFillDisabled,
+  ArrowRight,
+  ArrowRightFill,
+  ArrowRightFillDisabled,
+
+  // 버튼
+  ButtonAddImage,
+  ButtonAdd,
+  ButtonEdit,
+  ButtonRemove,
+
+  // Me / SNS
+  Me,
+  MeSchedule,
+  MeRegister,
+  MeReservations,
+  SnsFacebook,
+  SnsInstagram,
+  SnsYoutube,
+  SnsX,
+
+  // 상태
+  CheckMark,
+  CheckMarkFill,
+  Close,
+  CloseFill,
+  DotSmallBlue,
+  DotSmallGreen,
+  DotSmallRed,
+  StarEmpty,
+  StarFill,
+  Visibility,
+  VisibilityOff,
+
+  // UI
+  Calendar,
+  MenuDot,
+  NotificationBell,
+  NotingPage,
+  Search,
+};
