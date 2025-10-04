008994754c60a2b18fed9c444ad2c4d2d0105f8d
Jiii-Eun <mh030231@gmail.com>
2025-10-04 10:57:52 +0900

    Feat: 공통 버튼 컴포넌트

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { ButtonHTMLAttributes } from "react";
+
+import { cn } from "@/lib/cn";
+
+type ButtonVariant = "b" | "w" | "g";
+// b: nomad-black, w: white, g: green-500
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: ButtonVariant;
+  className?: string;
+  isdisabled?: boolean;
+}
+
+export default function Button({
+  children,
+  variant = "b",
+  className,
+  isdisabled,
+  ...props
+}: ButtonProps) {
+  // default: r-6 , 폰트 16px, 굵기 medium
+  const baseStyle =
+    "inline-flex justify-center items-center transition-all duration-150 rounded-b-md text-lg font-medium";
+
+  const variantStyle = {
+    b: "bg-brand-nomad-black text-white hover:bg-brand-gray-500",
+    w: "bg-white text-brand-nomad-black border border-brand-nomad-black hover:bg-brand-gray-200",
+    g: "bg-brand-green-500 text-white hover:brightness-95",
+  }[variant];
+
+  const disabledStyle =
+    "bg-brand-gray-600 text-brand-gray-300 cursor-not-allowed hover:brightness-100";
+
+  return (
+    <button
+      disabled={isdisabled}
+      className={cn(baseStyle, "", variantStyle, isdisabled && disabledStyle, className)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
