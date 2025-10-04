3195f06eb1f68b4b5f42985ed9919c6b98026ea9
jaewon8405 <wo19878405@gmail.com>
2025-10-04 22:02:04 +0900

    Desgin: 예약 시간대 선택 1차 기본 구조

--- src/app/me/activities/[id]/edit/page.tsx
+++ src/app/me/activities/[id]/edit/page.tsx
@@ -7,16 +7,18 @@ export default function Edit() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <DropDown handleClose={() => setIsOpen(false)}>
-        <DropDown.Trigger onClick={() => setIsOpen((v) => !v)}>⋮</DropDown.Trigger>
-        <DropDown.Menu isOpen={isOpen}>
-          <DropDown.Item onClick={() => setIsOpen(false)}>예약 신청</DropDown.Item>
-          <DropDown.Item onClick={() => setIsOpen(false)}>예약 취소</DropDown.Item>
-          <DropDown.Item onClick={() => setIsOpen(false)}>예약 승인</DropDown.Item>
-          <DropDown.Item onClick={() => setIsOpen(false)}>예약 거절</DropDown.Item>
-          <DropDown.Item onClick={() => setIsOpen(false)}>체험 완료</DropDown.Item>
-        </DropDown.Menu>
-      </DropDown>
+      <div>
+        <DropDown handleClose={() => setIsOpen(false)}>
+          <DropDown.Trigger onClick={() => setIsOpen((v) => !v)}>⋮</DropDown.Trigger>
+          <DropDown.Menu isOpen={isOpen}>
+            <DropDown.Item onClick={() => setIsOpen(false)}>예약 신청</DropDown.Item>
+            <DropDown.Item onClick={() => setIsOpen(false)}>예약 취소</DropDown.Item>
+            <DropDown.Item onClick={() => setIsOpen(false)}>예약 승인</DropDown.Item>
+            <DropDown.Item onClick={() => setIsOpen(false)}>예약 거절</DropDown.Item>
+            <DropDown.Item onClick={() => setIsOpen(false)}>체험 완료</DropDown.Item>
+          </DropDown.Menu>
+        </DropDown>
+      </div>
     </>
   );
 }
