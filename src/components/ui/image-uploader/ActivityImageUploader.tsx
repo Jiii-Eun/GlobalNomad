888c92f9d6276258848982cc21f4d3d4a1d40687
Jiii-Eun <mh030231@gmail.com>
2025-10-27 07:39:42 +0900

    Style:충돌해결

--- src/components/ui/image-uploader/ActivityImageUploader.tsx
+++ src/components/ui/image-uploader/ActivityImageUploader.tsx
@@ -60,7 +60,6 @@ export default function ActivityImageUploader({
       {canAddMore && (
         <Button
           type="button"
-          type="button"
           onClick={() => openFileDialog()}
           className={cn(
             listClass,
