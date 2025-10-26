d952455ea549fd06fb2b5433045c7ab0d69f6347
Jiii-Eun <mh030231@gmail.com>
2025-10-27 08:33:55 +0900

    Style: 충돌날 수 있는 코드 되돌림

--- src/app/me/components/ProfileSidebar.tsx
+++ src/app/me/components/ProfileSidebar.tsx
@@ -78,7 +78,7 @@ export default function ProfileSidebar({
   };
 
   return (
-    <aside className="rounded-12 border-brand-gray-300 mobile:hidden h-fit w-full max-w-96 shrink-0 border bg-white p-6">
+    <aside className="rounded-12 border-brand-gray-300 h-fit w-full max-w-96 border bg-white p-6">
       <div className="flex w-full flex-col items-center gap-6">
         <ProfileImageUploader initialUrl={initialProfileUrl} />
       </div>
