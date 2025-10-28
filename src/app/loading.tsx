393892bad7dfcdacd863e5eac384eb0fea1b1b78
Jiii-Eun <mh030231@gmail.com>
2025-10-29 03:20:56 +0900

    Style: 로딩 이미지 반응형크기 적용

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <Image
-        src="/images/loading.webp"
-        alt="로딩중..글로벌로 체험을 떠나는 비행기"
-        width={600}
-        height={600}
-      />
+      <div className="relative aspect-square min-w-2/5">
+        <Image
+          src="/images/loading.webp"
+          alt="로딩중.. 글로벌로 체험을 떠나는 비행기"
+          fill
+          className="object-contain"
+          priority={false}
+        />
+      </div>
     </div>
   );
 }
